aec44698608208064a10b7adf5dbd63a193a830b
d-verhnjak <darko.verhnjak@gmail.com>
2023-07-19 13:51:39 -0400

    Question model complete

--- server/models/Questions.js
+++ server/models/Questions.js
@@ -10,8 +10,11 @@ exports.getAll = async ({ product_id }) => {
 
 exports.add = async ({ product_id, body, name, email }) => {
   const date_written = new Date().getTime();
+  // const num = random(64)
+  // console.log(num)
+  // console.log(num.toString())
 
-  const params = [product_id, random(64), email, name, body, date_written, 0, 0];
+  const params = [product_id, random(32).toString(), email, name, body, date_written, 0, 0];
   console.log(params);
 
   const query = 'INSERT INTO qna.questions_by_product_id (product_id, id, asker_email, asker_name, body, date_written, helpful, reported) VALUES(?, ?, ?, ?, ?, ?, ?, ?)';
