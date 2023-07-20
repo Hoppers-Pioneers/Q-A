680cc89d294e320fc52309033d2dca3383d58a95
d-verhnjak <darko.verhnjak@gmail.com>
2023-07-20 14:09:29 -0400

    Getting questions working as they should now

--- server/controllers/Questions.js
+++ server/controllers/Questions.js
@@ -3,7 +3,7 @@ const questions = require('../models/Questions.js');
 exports.getAll = async (req, res) => {
   questions.getAll(req.query)
   .then((allResp) => {
-    res.status(200).send(allResp.rows);
+    res.status(200).send(allResp);
   })
   .catch( (err) => {
     console.error(err);
--- server/models/Questions.js
+++ server/models/Questions.js
@@ -1,11 +1,70 @@
 const client = require('../database');
-const random = require('random-bigint')
+const random = require('random-bigint');
 
 
 exports.getAll = async ({ product_id }) => {
-  const params = [0, product_id];
-  const query = 'SELECT * FROM questions_by_reported WHERE reported = ? AND product_id = ?';
-  return await client.execute(query, params, {prepare: true});
+  //------------------Question--------------------//
+  const queryQuest = 'SELECT * FROM questions_by_reported WHERE reported = 0 AND product_id = ?';
+
+  const questionData = await client.execute(queryQuest, [product_id], {prepare: true});
+
+  if (questionData.rows.length < 1) return {id: product_id, results: {}}
+
+  //--------------------Answers--------------------//
+  let queryAnswer = 'SELECT * FROM answers_by_reported WHERE reported = 0 AND question_id IN (';
+
+  const answerParams = [];
+  questionData.rows.forEach((question) => {
+    queryAnswer += '?,';
+    answerParams.push(question.id)
+  });
+  queryAnswer = queryAnswer.slice(0, queryAnswer.length - 1);
+  queryAnswer += ');';
+  let answerData;
+  if (answerParams.length > 0) {
+    answerData = await client.execute(queryAnswer, [...answerParams], {prepare: true});
+  }
+
+  //--------------------Photos--------------------//
+  let queryPhotos = 'SELECT * FROM photos_by_answer_id WHERE answer_id IN (';
+  const photoParams = [];
+  answerData.rows.forEach((answer) => {
+    queryPhotos += '?,';
+    photoParams.push(answer.id);
+  });
+  queryPhotos = queryPhotos.slice(0, queryPhotos.length - 1);
+  queryPhotos += ');';
+  let photoData;
+  if (photoParams.length > 0) {
+    photoData = await client.execute(queryPhotos, [...photoParams], {prepare: true})
+  }
+
+
+  //--------------------Compile------------------//
+
+  answerData.rows.forEach((answer) => {
+    if (!answer.photos) answer.photos = [];
+    photoData.rows.forEach((photo) => {
+      if (photo.answer_id == answer.id) answer.photos.push({id: photo.id, url: photo.url});
+    })
+  });
+
+  questionData.rows.forEach((question) => {
+    if (!question.answers) question.answers = {};
+    answerData.rows.forEach((answer) => {
+      if (answer.question_id == question.id) question.answers[answer.id] = answer;
+    })
+  });
+
+  console.log('RESULT', questionData.rows)
+
+
+  return (
+    {
+      id: product_id,
+      results: questionData.rows
+    }
+  )
 };
 
 exports.add = async ({ product_id, body, name, email }) => {
